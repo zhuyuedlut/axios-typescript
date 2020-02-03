fb0d11b1f1d130ce42958bcaf2c27357b5fd7074
zhuyuedlut <516872091@qq.com>
2020-02-03 16:58:47 +0800

    feat(complete transform url):

---
+++ src/helpers/url.ts
@@ -0,0 +1,49 @@
+import { isDate, isObject } from './utils'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+  const parts: string[] = []
+  Object.keys(params).forEach(key => {
+    let value = params[key]
+    if (value === null || value === 'undefined') {
+      return
+    }
+    let values: string[]
+    if (Array.isArray(value)) {
+      values = value
+      key = key + '[]'
+    } else {
+      values = [value]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+    let serializedParams = parts.join('&')
+    if (serializedParams) {
+      const markIndex = url.indexOf('#')
+      if (markIndex !== -1) {
+        url = url.slice(0, markIndex)
+      }
+      url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+    }
+  })
+  return url
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { AxiosRequestConfig } from './types/index'
 import { xhr } from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
   xhr(config)
+  processConfig(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): void {
+  const { url, params } = config
+  buildURL(url, params)
 }
 
 export default axios
